02d4fc0c2912070596dbdcb227c355237331740a
zaviro <1264166738a@gmail.com>
2026-07-31 02:48:48 +0000

    feat(ssh): configure GitHub over port 443

--- hosts/legion-wsl/home.nix
+++ hosts/legion-wsl/home.nix
@@ -7,4 +7,19 @@
     codexPackage
     pkgs.tailscale
   ];
+
+  programs.ssh = {
+    enable = true;
+
+    settings."github.com" = {
+      HostName = "ssh.github.com";
+      User = "git";
+      Port = 443;
+      IdentityFile = "~/.ssh/id_ed25519";
+      IdentitiesOnly = true;
+      AddKeysToAgent = "yes";
+    };
+  };
+
+  services.ssh-agent.enable = true;
 }
