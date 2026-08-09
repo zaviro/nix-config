4057e759b0e501d494509064bad77066720240f7
zaviro <1264166738a@gmail.com>
2026-08-10 05:20:10 +0800

    feat(ghostty): configure atlas terminal appearance

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -2,6 +2,12 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
-    settings.fullscreen = true;
+    settings = {
+      font-family = "JetBrains Mono";
+      copy-on-select = "clipboard";
+      cursor-style = "bar";
+      cursor-style-blink = false;
+      fullscreen = true;
+    };
   };
 }
