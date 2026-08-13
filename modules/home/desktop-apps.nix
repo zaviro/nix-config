ef51ceb4ba9e801f7b54dc75d179c7378aba8a0b
zaviro <1264166738a@gmail.com>
2026-08-13 12:43:50 +0800

    feat(home): add media playback and download tools

--- modules/home/desktop-apps.nix
+++ modules/home/desktop-apps.nix
@@ -4,11 +4,13 @@
   home.packages = with pkgs; [
     firefox
     google-chrome
+    mpv
     obsidian
     readest
     rustdesk
     spotify
     tauon
+    yt-dlp
   ];
 
   dconf.settings."org/gnome/desktop/default-applications/terminal" = {
